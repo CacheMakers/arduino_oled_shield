1b231215893b7e3cbbc4484743e896f2b9518d7d
johnathanlp <johnathanlpowell@gmail.com>
2019-04-16 20:26:05 -0600

    added jump and rollin donut

---
+++ jump/jump.ino
@@ -0,0 +1,107 @@
+//#include <SPI.h>
+//#include <Wire.h>
+#include <Adafruit_SSD1306.h>
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+int buzz = 4;
+
+int bA = 5;
+int bX = 6;
+int bB = 7;
+int bY = 8;
+
+int rgt = 9;
+int up  = 10;
+int dwn = 11;
+int lft = 12;
+
+byte pX = 32;
+byte pY = 60;
+byte pW = 8; 
+byte pH = 8;
+byte vY = 0;
+int oX = -10;
+int oX2 = -10;
+byte oW = 2;
+byte oH = 20;
+
+int score = 0;
+
+void setup()   
+{                
+  Serial.begin(9600);
+
+  // use internal 3.3v, and set the i2c address of the display
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+
+  // set buzzer to output, and each of the buttons to input_pullup
+  pinMode(buzz , OUTPUT);
+  for(int i=5; i<=12; i++)
+  {
+    pinMode(i, INPUT_PULLUP);
+  }
+}
+
+void loop() 
+{
+   if(digitalRead(bX) == LOW && pY >= 60)
+   {
+
+     vY = -8;
+   }
+   pY += vY;
+   vY += 1;
+   if(pY >= 60)
+   {
+    pY = 60;
+    vY = 0;
+   }
+
+   if(oX <= -10)
+   {
+     if(random(2) == 1)
+     {
+      oX = 128+random(50);
+     }
+   } 
+   else
+   {
+      oX -= 3;
+   }
+
+   if(o2X <= -10)
+   {
+     if(random(2) == 1)
+     {
+      o2X =128+random(50);
+     }
+   } 
+   else
+   {
+      o2X -= 3;
+   }
+
+
+if((pX-pW<oX && oX-oW<pX && pY>40)||(pX-pW<oX2 && oX2-oW<pX && pY>40))
+{
+  Serial.println("crash");
+  score = 0; 
+}
+else
+{
+  score++;
+}
+   
+   
+  display.clearDisplay();
+  display.fillRect (pX-pW,pY-pH,  pW,  pH,WHITE);
+  display.fillRect(0,     60, 128, 4, WHITE);
+  display.fillRect(oX,   40,   oW,   oH, WHITE);
+  display.fillRect(oX2,  40,   oW,   oH, WHITE);
+  display.setCursor(100,1);
+  display.setTextColor(WHITE);
+  display.println(score);
+  display.display();
+}
---
+++ rollin_donut/rollin_donut.ino
@@ -0,0 +1,65 @@
+//#include <SPI.h>
+//#include <Wire.h>
+#include <Adafruit_SSD1306.h>
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+byte buzz = 4;
+
+byte bA = 5;
+byte bX = 6;
+byte bB = 7;
+byte bY = 8;
+
+byte rgt = 9;
+byte up  = 10;
+byte dwn = 11;
+byte lft = 12;
+
+int x = -32;
+int t = 0;
+
+int getX(int t, int r)
+{
+  return r*cos(t*6.28/360);
+}
+
+int getY(int t, int r)
+{
+  return r*sin(t*6.28/360);
+}
+
+void setup()   
+{                
+  Serial.begin(9600);
+
+  // use internal 3.3v, and set the i2c address of the display
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+
+  // set buzzer to output, and each of the buttons to input_pullup
+  pinMode(buzz , OUTPUT);
+  for(int i=5; i<=12; i++)
+  {
+    pinMode(i, INPUT_PULLUP);
+  }
+  display.clearDisplay();
+}
+
+void loop() 
+{
+  x++;
+  t+=2;
+  if(x >=160)
+  {
+    x=-32;
+  }
+  
+  display.clearDisplay();
+  display.fillCircle(x,32,30,WHITE);
+  display.fillCircle(x+getX(t,20), 32+getY(t,20),5,BLACK);
+  display.fillCircle(x+getX(t+120,20), 32+getY(t+120,20),5,BLACK);
+  display.fillCircle(x+getX(t+240,20), 32+getY(t+240,20),5,BLACK);
+  display.fillCircle(x,32,10,BLACK);
+  display.display();
+}
